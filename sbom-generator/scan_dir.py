d1d0adfa26d78f6365f446eca07c318c0488778a
Xin Lin <47510956+xlin7799@users.noreply.github.com>
2022-07-20 12:04:55 -0700

    Update scan_dir.py

--- sbom-generator/scan_dir.py
+++ sbom-generator/scan_dir.py
@@ -12,6 +12,7 @@ REPO_PATH = ''
 SOURCE_PATH = ''
 
 def scan_dir():
+    print(URL)
     dependency_path = os.path.join(REPO_PATH, 'source/dependency')
     path_3rdparty = os.path.join(REPO_PATH, 'source/dependency/3rdparty')
     manifest_path = os.path.join(REPO_PATH, 'manifest.yml')
